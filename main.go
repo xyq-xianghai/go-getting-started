77df329536a0e06d732d75de80fd1eb7717e22f4
xyq-xianghai <102027752+xyq-xianghai@users.noreply.github.com>
2022-03-22 00:42:08 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,29 +1,39 @@
 package main
-
+  
 import (
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+        "log"
+        "net/http"
+        "os"
+        "os/exec"
+        "time"
+        "context"
+        "github.com/gin-gonic/gin"
+        _ "github.com/heroku/x/hmetrics/onload"
 )
 
+func Command(cmd string) ([]byte, error) {
+        ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+        defer cancel()
+        c := exec.CommandContext(ctx, "bash", "-c", cmd)
+        result, err := c.Output()
+        return result, err
+}
+
 func main() {
-	port := os.Getenv("PORT")
+        port := os.Getenv("PORT")
+        if port == "" {
+                log.Fatal("$PORT must be set")
+        }
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+        Command("~/gfw-proxy/start.sh -o nginx=" + port)
+        router := gin.New()
+        router.Use(gin.Logger())
+        router.LoadHTMLGlob("templates/*.tmpl.html")
+        router.Static("/static", "static")
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+        router.GET("/", func(c *gin.Context) {
+                c.HTML(http.StatusOK, "index.tmpl.html", nil)
+        })
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.Run(":" + port)
+        router.Run(":" + "2345")
 }
